791dbd14f21cf8a47ff2c4f0f6672eb3c3942e52
AVEY NATA <76590363+AVEYNT@users.noreply.github.com>
2022-04-04 14:16:09 +0700

    blomklar, masi di vs studio

---
+++ Tugas2PL.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSHARP-LEARN
+{
+    class Program
+    {
+        static void Main(String{args})
+        {
+            
+        }
+    }
+}
